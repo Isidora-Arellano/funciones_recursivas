e4d633a479ea99f16f8a52a7be46611000d7fc63
Isidora <isidoravargas1804@gmail.com>
2025-05-09 17:41:55 -0400

    add a multiplication fuction

--- recursives.py
+++ recursives.py
@@ -10,13 +10,23 @@ def countTotalNumber(array):
         return 0
     else:
         return 1 + countTotalNumber(array[1:])
+    
+def multiNumber(array):
+    if not array:
+        return 1
+    else:
+        return array[0]* multiNumber(array[1:])
 
 numbers = [1,2,3,4,5]
 result = addRecursive(numbers)
 print("Resultado",result)
 
 
-
 numbers2 = [1,2,3,4,5,4,7,1,9,2,3]
 result2 = countTotalNumber(numbers2)
 print("Resultado",result2)
+
+
+numbers3 = [1,2,3,4,3,5]
+result3 = multiNumber(numbers3)
+print("Resultado",result3)
